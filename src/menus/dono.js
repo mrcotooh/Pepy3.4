39f5ea15f5ed423e33bc103153a2a9f938b9fe7c
Guilherme Portilho <58198561+mrcotooh@users.noreply.github.com>
2021-03-18 20:27:09 -0300

    Create dono.js

---
+++ src/menus/dono.js
@@ -0,0 +1,86 @@
+const outros = () => { 
+
+	return `    ♟ Pepy Bot ♟
+
+    
+
+🏮Versão 3.5 
+
+🏮Dono: Guilherme
+
+🏮STATUS: ON
+
+🗂〘 OUTROS 〙🗂
+
+ 📌${prefix}linkgp
+
+ 📌${prefix}simih [1/0]
+
+ 📌${prefix}marcar
+
+ 📌${prefix}add [@]
+
+ 📌${prefix}banir [@]
+
+ 📌${prefix}promover [@]
+
+ 📌${prefix}rebaixar
+
+ 📌${prefix}admins
+
+ 📌${prefix}marcar2
+
+ 📌${prefix}bc [texto] (ele faz uma ™)
+
+
+
+ 📌${prefix}bloqueados
+
+ 📌${prefix}bloquear [@]
+
+ 📌${prefix}desbloquear [@]
+
+ 📌${prefix}limpar
+
+ 📌${prefix}bc [ 📌texto📌 ]
+
+ 📌${prefix}bemvindo [1/0]
+
+ 📌${prefix}clonar [@]
+
+ 📌${prefix}help1
+
+ 📌${prefix}dono
+
+ 📌${prefix}owner
+
+ 📌${prefix}tts [texto]
+
+ 📌${prefix}setnome
+
+ 📌${prefix}termux
+
+ 📌${prefix}setfoto
+
+ 📌${prefix}grupoinfo
+
+ 📌${prefix}ytmp4
+
+ 📌${prefix}bomdia
+
+ 📌${prefix
+
+
+      🏮 DONO 🏮
+
+🏮NOME: Guilherme
+
+🏮INSTA: hoshigaki_guilherme
+
+🏮WPP: +5519999897566
+
+ ♟ Pepy Bot ♟`
+
+}
+
+exports.outros = outros
