6bd2bf2d35ef532d74a7e7c67ba9791b67de7620
Guilherme Portilho <58198561+mrcotooh@users.noreply.github.com>
2021-03-18 16:39:24 -0300

    Update premium.js

--- src/menus/premium.js
+++ src/menus/premium.js
@@ -1,40 +1,37 @@
 const premium = () => { 
 	return `
- INFO 
-
-    [ PEPYTO Bot ]
-馃搶Vers茫o 3.4
-馃搶Dono: Guilherme
-馃搶Whatsapp: https://wa.me/+5519999897566
-馃搶STATUS: ON
+    ♟ Pepy Bot ♟
+📌Versão 3.5 
+📌Dono: Guilherme
+📌Whatsapp: https://wa.me/+5519999897566
+📌STATUS: ON
 
 
-🗂〘 PREMIUM 〙🗂
+🗂 PREMIUM 🗂
 
- 📌${prefix}dado
- 📌${prefix}cekvip
- 📌${prefix}premiumlist
- 📌${prefix}delete
- 📌${prefix}modapk
- 📌${prefix}indo10
- 📌${prefix}daftarvip [para virar Premium]
- 📌${prefix}qrcode
- 📌${prefix}chentai
- 📌${prefix}gcpf
- 📌${prefix}gbin
- 📌${prefix}pack
- 📌${prefix}destrava
- 📌${prefix}gpessoa
+📌${prefix}dado
+📌${prefix}cekvip
+📌${prefix}premiumlist
+📌${prefix}delete
+📌${prefix}modapk
+📌${prefix}indo10
+📌${prefix}daftarvip [para virar Premium]
+📌${prefix}qrcode
+📌${prefix}chentai
+📌${prefix}gcpf
+📌${prefix}gbin
+📌${prefix}pack
+📌${prefix}destrava
+📌${prefix}gpessoa
  
 
-      馃彯DONO 馃彯
+      📌 DONO 📌
 
-馃彯NOME: Guilherme
-馃彯INSTA: https://instagram.com/hoshigaki_guilherme?igshid=cef78b814ce3
-馃彯WPP: https://wa.me/+5519999897566
-馃彯${prefix}dono  para mais info
+🏮NOME: Guilherme
+🏮INSTA: hoshigaki_guilherme
+🏮WPP: +5519999897566
 
- [ Pepy Bot ]`
+ ♟ Pepy Bot ♟`
 }
 
 exports.premium = premium
