748af7ab523e1e0073d79b5b2e2d7721dab48ff2
Guilherme Portilho <58198561+mrcotooh@users.noreply.github.com>
2021-03-18 16:22:50 -0300

    Update outros.js

--- src/menus/outros.js
+++ src/menus/outros.js
@@ -1,12 +1,10 @@
 const outros = () => { 
 	return `
- INFO 
-
-    [ PEPYTO Bot ]
-馃搶Vers茫o 3.4
-馃搶Dono: Guilherme
-馃搶Whatsapp: https://wa.me/+5519999897566
-馃搶STATUS: ON
+    ♟ Pepy Bot ♟
+    
+🏮Versão 3.5 
+🏮Dono: Guilherme
+🏮STATUS: ON
 
 🗂〘 OUTROS 〙🗂
 
@@ -52,14 +50,13 @@ const outros = () => {
  📌${prefix}animemenu
 
 
-      馃彯DONO 馃彯
+      🏮 DONO 🏮
 
-馃彯NOME: Guilherme
-馃彯INSTA: https://instagram.com/hoshigaki_guilherme?igshid=cef78b814ce3
-馃彯WPP: https://wa.me/+5519999897566
-馃彯${prefix}dono  para mais info
+🏮NOME: Guilherme
+🏮INSTA: hoshigaki_guilherme
+🏮WPP: +5519999897566
 
- [ Pepy Bot ]`
+ ♟ Pepy Bot ♟`
 }
 
 exports.outros = outros
